ac186d0dbf8c51e9450c68a81947b3a0f4386927
Sebastián Barreto <luscorreab@udistrital.edu.co>
2026-05-25 13:28:47 -0500

    push inicial

---
+++ Backend-Multi-Triatlon/Triatleta/src/main/java/pa/microservicios/Triatleta/TriatletaApplication.java
@@ -0,0 +1,13 @@
+package pa.microservicios.Triatleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriatletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriatletaApplication.class, args);
+	}
+
+}
---
+++ Backend-Multi-Triatlon/Triatleta/src/test/java/pa/microservicios/Triatleta/TriatletaApplicationTests.java
@@ -0,0 +1,13 @@
+package pa.microservicios.Triatleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriatletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
